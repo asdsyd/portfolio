f69c719f8681b775ab808d387c0847f47724c404
asdsyd <ashasd97@gmail.com>
2024-06-27 20:20:04 +0530

    initial navbar: need to work on flex

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,40 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+      <header className="w-full bg-blue-800 text-white p-4 flex justify-between items-center rounded-tl-[55px] rounded-tr-[55px] shadow-2xl">
+        <h1 className="text-2xl font-bold px-10">ASAD SAYEED</h1>
+        <nav className="space-x-4 px-10">
+          <a href="#" className="hover:underline">PORTFOLIO</a>
+          <a href="#" className="hover:underline">RESUME</a>
+          <a href="#" className="hover:underline">ABOUT</a>
+        </nav>
+      </header>
+
+      <div className="flex flex-col items-center justify-center">
+        <img src="/apple-developer.png"></img>
+      </div>
+
+      <div className="flex flex-col items-center justify-center">
+        <img src="/tech-stack.png"></img>
+      </div>
+      
+
+      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+        <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          src="/apple-developer.png"
+          alt="Next.js Logo"
+          width={180}
+          height={100}
+          priority
+        />
+      </div>
+
+
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
+          Get started by does it do live editing yes it does&nbsp;
           <code className="font-mono font-bold">app/page.tsx</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
@@ -26,6 +57,11 @@ export default function Home() {
             />
           </a>
         </div>
+      </div>
+
+      <div className="flex justify-center -space-x-14 blur-2xl">
+        <div className="w-32 h-32 mix-blend-multiply bg-blue-400 rounded-full"></div>
+        <div className="w-32 h-32 mix-blend-multiply bg-pink-400 rounded-full"></div>
       </div>
 
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
