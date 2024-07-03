8eaa8878740b64d8110213a87f75cc0525d0a0c5
asdsyd <ashasd97@gmail.com>
2024-07-04 01:09:51 +0530

    resume href fix for mobile view

--- app/page.tsx
+++ app/page.tsx
@@ -165,7 +165,7 @@ export default function Example() {
                 </Disclosure>
                 <a
                   target='_blank'
-                  href="#"
+                  href="https://drive.google.com/drive/folders/1hOYTr1L1v7YZJmffBZC8jGLB4V5KU4Px"
                   className="-mx-3 block rounded-full px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-blue-100"
                 >
                   RESUME
